23c77ca4100a05b1931ff02f311fbdd177c33caa
hunterbecton <hunter@hunterbecton.com>
2021-08-04 13:38:00 -0400

    First

--- pages/index.js
+++ pages/index.js
@@ -1,82 +1,168 @@
-import Head from 'next/head'
-
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
-
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="p-3 font-mono text-lg bg-gray-100 rounded-md">
-            pages/index.js
-          </code>
-        </p>
-
-        <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+    <div className='relative py-16 bg-white overflow-hidden'>
+      <div className='relative px-4 sm:px-6 lg:px-8'>
+        <div className='text-lg max-w-prose mx-auto'>
+          <h1>
+            <span className='block text-base text-center text-pink-600 font-semibold tracking-wide uppercase'>
+              Course
+            </span>
+            <span className='mt-2 block text-3xl text-center leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl'>
+              Build a full-stack app with Next.js
+            </span>
+          </h1>
+          <p className='mt-8 text-xl text-gray-500 leading-8'>
+            Aliquet nec orci mattis amet quisque ullamcorper neque, nibh sem. At
+            arcu, sit dui mi, nibh dui, diam eget aliquam. Quisque id at vitae
+            feugiat egestas ac. Diam nulla orci at in viverra scelerisque eget.
+            Eleifend egestas fringilla sapien.
+          </p>
         </div>
-      </main>
-
-      <footer className="flex items-center justify-center w-full h-24 border-t">
-        <a
-          className="flex items-center justify-center"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="h-4 ml-2" />
-        </a>
-      </footer>
+        <div className='mt-6 prose prose-pink prose-lg text-gray-500 mx-auto'>
+          <p>
+            Faucibus commodo massa rhoncus, volutpat. <strong>Dignissim</strong>{' '}
+            sed. Mattis mauris semper sed amet vitae sed turpis id. Id dolor
+            praesent donec est. Odio penatibus risus viverra tellus varius sit
+            neque erat velit. Faucibus commodo massa rhoncus, volutpat.
+            Dignissim sed eget risus enim. <a href='#'>Mattis mauris semper</a>{' '}
+            sed amet vitae sed turpis id.
+          </p>
+          <ul>
+            <li>Quis elit egestas venenatis mattis dignissim.</li>
+            <li>
+              Cras cras lobortis vitae vivamus ultricies facilisis tempus.
+            </li>
+            <li>Orci in sit morbi dignissim metus diam arcu pretium.</li>
+          </ul>
+          <p>
+            Quis semper vulputate aliquam venenatis egestas sagittis quisque
+            orci. Donec commodo sit viverra aliquam porttitor ultrices gravida
+            eu. Tincidunt leo, elementum mattis elementum ut nisl, justo, amet,
+            mattis. Nunc purus, diam commodo tincidunt turpis. Amet, duis sed
+            elit interdum dignissim.
+          </p>
+          <h2>From beginner to expert in 30 days</h2>
+          <p>
+            Id orci tellus laoreet id ac. Dolor, aenean leo, ac etiam consequat
+            in. Convallis arcu ipsum urna nibh. Pharetra, euismod vitae interdum
+            mauris enim, consequat vulputate nibh. Maecenas pellentesque id sed
+            tellus mauris, ultrices mauris. Tincidunt enim cursus ridiculus mi.
+            Pellentesque nam sed nullam sed diam turpis ipsum eu a sed convallis
+            diam.
+          </p>
+          <blockquote>
+            <p>
+              Sagittis scelerisque nulla cursus in enim consectetur quam. Dictum
+              urna sed consectetur neque tristique pellentesque. Blandit amet,
+              sed aenean erat arcu morbi.
+            </p>
+          </blockquote>
+          <p>
+            Faucibus commodo massa rhoncus, volutpat. Dignissim sed eget risus
+            enim. Mattis mauris semper sed amet vitae sed turpis id. Id dolor
+            praesent donec est. Odio penatibus risus viverra tellus varius sit
+            neque erat velit.
+          </p>
+          <figure>
+            <img
+              className='w-full rounded-lg'
+              src='https://images.unsplash.com/photo-1531297484001-80022131f5a1?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1000&q=80'
+              alt=''
+              width={1000}
+              height={600}
+            />
+            <figcaption>
+              Sagittis scelerisque nulla cursus in enim consectetur quam.
+            </figcaption>
+          </figure>
+          <h2>Everything you need to get up and running</h2>
+          <p>
+            Purus morbi dignissim senectus mattis <a href='#'>adipiscing</a>.
+            Amet, massa quam varius orci dapibus volutpat cras. In amet eu
+            ridiculus leo sodales cursus tristique. Tincidunt sed tempus ut
+            viverra ridiculus non molestie. Gravida quis fringilla amet eget dui
+            tempor dignissim. Facilisis auctor venenatis varius nunc, congue
+            erat ac. Cras fermentum convallis quam.
+          </p>
+          <p>
+            Faucibus commodo massa rhoncus, volutpat. Dignissim sed eget risus
+            enim. Mattis mauris semper sed amet vitae sed turpis id. Id dolor
+            praesent donec est. Odio penatibus risus viverra tellus varius sit
+            neque erat velit.
+          </p>
+          <p>
+            Faucibus commodo massa rhoncus, volutpat. <strong>Dignissim</strong>{' '}
+            sed. Mattis mauris semper sed amet vitae sed turpis id. Id dolor
+            praesent donec est. Odio penatibus risus viverra tellus varius sit
+            neque erat velit. Faucibus commodo massa rhoncus, volutpat.
+            Dignissim sed eget risus enim. <a href='#'>Mattis mauris semper</a>{' '}
+            sed amet vitae sed turpis id.
+          </p>
+          <ul>
+            <li>Quis elit egestas venenatis mattis dignissim.</li>
+            <li>
+              Cras cras lobortis vitae vivamus ultricies facilisis tempus.
+            </li>
+            <li>Orci in sit morbi dignissim metus diam arcu pretium.</li>
+          </ul>
+          <p>
+            Quis semper vulputate aliquam venenatis egestas sagittis quisque
+            orci. Donec commodo sit viverra aliquam porttitor ultrices gravida
+            eu. Tincidunt leo, elementum mattis elementum ut nisl, justo, amet,
+            mattis. Nunc purus, diam commodo tincidunt turpis. Amet, duis sed
+            elit interdum dignissim.
+          </p>
+          <h2>The help you need any time you feel stuck</h2>
+          <p>
+            Id orci tellus laoreet id ac. Dolor, aenean leo, ac etiam consequat
+            in. Convallis arcu ipsum urna nibh. Pharetra, euismod vitae interdum
+            mauris enim, consequat vulputate nibh. Maecenas pellentesque id sed
+            tellus mauris, ultrices mauris. Tincidunt enim cursus ridiculus mi.
+            Pellentesque nam sed nullam sed diam turpis ipsum eu a sed convallis
+            diam.
+          </p>
+          <blockquote>
+            <p>
+              Sagittis scelerisque nulla cursus in enim consectetur quam. Dictum
+              urna sed consectetur neque tristique pellentesque. Blandit amet,
+              sed aenean erat arcu morbi.
+            </p>
+          </blockquote>
+          <p>
+            Faucibus commodo massa rhoncus, volutpat. Dignissim sed eget risus
+            enim. Mattis mauris semper sed amet vitae sed turpis id. Id dolor
+            praesent donec est. Odio penatibus risus viverra tellus varius sit
+            neque erat velit.
+          </p>
+          <figure>
+            <img
+              className='w-full rounded-lg'
+              src='https://images.unsplash.com/photo-1530893609608-32a9af3aa95c?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1000&q=80'
+              alt=''
+              width={1000}
+              height={600}
+            />
+            <figcaption>
+              Sagittis scelerisque nulla cursus in enim consectetur quam.
+            </figcaption>
+          </figure>
+          <h2>A thriving community of developers</h2>
+          <p>
+            Purus morbi dignissim senectus mattis <a href='#'>adipiscing</a>.
+            Amet, massa quam varius orci dapibus volutpat cras. In amet eu
+            ridiculus leo sodales cursus tristique. Tincidunt sed tempus ut
+            viverra ridiculus non molestie. Gravida quis fringilla amet eget dui
+            tempor dignissim. Facilisis auctor venenatis varius nunc, congue
+            erat ac. Cras fermentum convallis quam.
+          </p>
+          <p>
+            Faucibus commodo massa rhoncus, volutpat. Dignissim sed eget risus
+            enim. Mattis mauris semper sed amet vitae sed turpis id. Id dolor
+            praesent donec est. Odio penatibus risus viverra tellus varius sit
+            neque erat velit.
+          </p>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
