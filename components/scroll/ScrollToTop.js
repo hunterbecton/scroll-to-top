ee7e2f22e875ea8057639a7699a934f26bf32867
hunterbecton <hunter@hunterbecton.com>
2021-08-04 13:53:34 -0400

    Added scroll to top component

---
+++ components/scroll/ScrollToTop.js
@@ -0,0 +1,46 @@
+import { useEffect, useState } from 'react';
+import { BiArrowFromBottom } from 'react-icons/bi';
+
+import { classNames } from '../../utils';
+
+export const ScrollToTop = () => {
+  const [isVisible, setIsVisible] = useState(false);
+
+  const toggleVisibility = () => {
+    if (window.pageYOffset > 300) {
+      setIsVisible(true);
+    } else {
+      setIsVisible(false);
+    }
+  };
+
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    });
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', toggleVisibility);
+
+    return () => {
+      window.removeEventListener('scroll', toggleVisibility);
+    };
+  }, []);
+
+  return (
+    <div className='fixed bottom-2 right-2'>
+      <button
+        type='button'
+        onClick={scrollToTop}
+        className={classNames(
+          isVisible ? 'opacity-100' : 'opacity-0',
+          'inline-flex items-center p-3 border border-transparent rounded-full shadow-sm text-white bg-pink-600 transition-opacity hover:bg-pink-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-pink-500'
+        )}
+      >
+        <BiArrowFromBottom className='h-6 w-6' aria-hidden='true' />
+      </button>
+    </div>
+  );
+};
--- pages/index.js
+++ pages/index.js
@@ -1,168 +1,173 @@
+import { ScrollToTop } from '../components';
+
 export default function Home() {
   return (
-    <div className='relative py-16 bg-white overflow-hidden'>
-      <div className='relative px-4 sm:px-6 lg:px-8'>
-        <div className='text-lg max-w-prose mx-auto'>
-          <h1>
-            <span className='block text-base text-center text-pink-600 font-semibold tracking-wide uppercase'>
-              Course
-            </span>
-            <span className='mt-2 block text-3xl text-center leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl'>
-              Build a full-stack app with Next.js
-            </span>
-          </h1>
-          <p className='mt-8 text-xl text-gray-500 leading-8'>
-            Aliquet nec orci mattis amet quisque ullamcorper neque, nibh sem. At
-            arcu, sit dui mi, nibh dui, diam eget aliquam. Quisque id at vitae
-            feugiat egestas ac. Diam nulla orci at in viverra scelerisque eget.
-            Eleifend egestas fringilla sapien.
-          </p>
-        </div>
-        <div className='mt-6 prose prose-pink prose-lg text-gray-500 mx-auto'>
-          <p>
-            Faucibus commodo massa rhoncus, volutpat. <strong>Dignissim</strong>{' '}
-            sed. Mattis mauris semper sed amet vitae sed turpis id. Id dolor
-            praesent donec est. Odio penatibus risus viverra tellus varius sit
-            neque erat velit. Faucibus commodo massa rhoncus, volutpat.
-            Dignissim sed eget risus enim. <a href='#'>Mattis mauris semper</a>{' '}
-            sed amet vitae sed turpis id.
-          </p>
-          <ul>
-            <li>Quis elit egestas venenatis mattis dignissim.</li>
-            <li>
-              Cras cras lobortis vitae vivamus ultricies facilisis tempus.
-            </li>
-            <li>Orci in sit morbi dignissim metus diam arcu pretium.</li>
-          </ul>
-          <p>
-            Quis semper vulputate aliquam venenatis egestas sagittis quisque
-            orci. Donec commodo sit viverra aliquam porttitor ultrices gravida
-            eu. Tincidunt leo, elementum mattis elementum ut nisl, justo, amet,
-            mattis. Nunc purus, diam commodo tincidunt turpis. Amet, duis sed
-            elit interdum dignissim.
-          </p>
-          <h2>From beginner to expert in 30 days</h2>
-          <p>
-            Id orci tellus laoreet id ac. Dolor, aenean leo, ac etiam consequat
-            in. Convallis arcu ipsum urna nibh. Pharetra, euismod vitae interdum
-            mauris enim, consequat vulputate nibh. Maecenas pellentesque id sed
-            tellus mauris, ultrices mauris. Tincidunt enim cursus ridiculus mi.
-            Pellentesque nam sed nullam sed diam turpis ipsum eu a sed convallis
-            diam.
-          </p>
-          <blockquote>
-            <p>
-              Sagittis scelerisque nulla cursus in enim consectetur quam. Dictum
-              urna sed consectetur neque tristique pellentesque. Blandit amet,
-              sed aenean erat arcu morbi.
+    <>
+      <div className='relative py-16 bg-white overflow-hidden'>
+        <div className='relative px-4 sm:px-6 lg:px-8'>
+          <div className='text-lg max-w-prose mx-auto'>
+            <h1>
+              <span className='block text-base text-center text-pink-600 font-semibold tracking-wide uppercase'>
+                Course
+              </span>
+              <span className='mt-2 block text-3xl text-center leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl'>
+                Build a full-stack app with Next.js
+              </span>
+            </h1>
+            <p className='mt-8 text-xl text-gray-500 leading-8'>
+              Aliquet nec orci mattis amet quisque ullamcorper neque, nibh sem.
+              At arcu, sit dui mi, nibh dui, diam eget aliquam. Quisque id at
+              vitae feugiat egestas ac. Diam nulla orci at in viverra
+              scelerisque eget. Eleifend egestas fringilla sapien.
             </p>
-          </blockquote>
-          <p>
-            Faucibus commodo massa rhoncus, volutpat. Dignissim sed eget risus
-            enim. Mattis mauris semper sed amet vitae sed turpis id. Id dolor
-            praesent donec est. Odio penatibus risus viverra tellus varius sit
-            neque erat velit.
-          </p>
-          <figure>
-            <img
-              className='w-full rounded-lg'
-              src='https://images.unsplash.com/photo-1531297484001-80022131f5a1?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1000&q=80'
-              alt=''
-              width={1000}
-              height={600}
-            />
-            <figcaption>
-              Sagittis scelerisque nulla cursus in enim consectetur quam.
-            </figcaption>
-          </figure>
-          <h2>Everything you need to get up and running</h2>
-          <p>
-            Purus morbi dignissim senectus mattis <a href='#'>adipiscing</a>.
-            Amet, massa quam varius orci dapibus volutpat cras. In amet eu
-            ridiculus leo sodales cursus tristique. Tincidunt sed tempus ut
-            viverra ridiculus non molestie. Gravida quis fringilla amet eget dui
-            tempor dignissim. Facilisis auctor venenatis varius nunc, congue
-            erat ac. Cras fermentum convallis quam.
-          </p>
-          <p>
-            Faucibus commodo massa rhoncus, volutpat. Dignissim sed eget risus
-            enim. Mattis mauris semper sed amet vitae sed turpis id. Id dolor
-            praesent donec est. Odio penatibus risus viverra tellus varius sit
-            neque erat velit.
-          </p>
-          <p>
-            Faucibus commodo massa rhoncus, volutpat. <strong>Dignissim</strong>{' '}
-            sed. Mattis mauris semper sed amet vitae sed turpis id. Id dolor
-            praesent donec est. Odio penatibus risus viverra tellus varius sit
-            neque erat velit. Faucibus commodo massa rhoncus, volutpat.
-            Dignissim sed eget risus enim. <a href='#'>Mattis mauris semper</a>{' '}
-            sed amet vitae sed turpis id.
-          </p>
-          <ul>
-            <li>Quis elit egestas venenatis mattis dignissim.</li>
-            <li>
-              Cras cras lobortis vitae vivamus ultricies facilisis tempus.
-            </li>
-            <li>Orci in sit morbi dignissim metus diam arcu pretium.</li>
-          </ul>
-          <p>
-            Quis semper vulputate aliquam venenatis egestas sagittis quisque
-            orci. Donec commodo sit viverra aliquam porttitor ultrices gravida
-            eu. Tincidunt leo, elementum mattis elementum ut nisl, justo, amet,
-            mattis. Nunc purus, diam commodo tincidunt turpis. Amet, duis sed
-            elit interdum dignissim.
-          </p>
-          <h2>The help you need any time you feel stuck</h2>
-          <p>
-            Id orci tellus laoreet id ac. Dolor, aenean leo, ac etiam consequat
-            in. Convallis arcu ipsum urna nibh. Pharetra, euismod vitae interdum
-            mauris enim, consequat vulputate nibh. Maecenas pellentesque id sed
-            tellus mauris, ultrices mauris. Tincidunt enim cursus ridiculus mi.
-            Pellentesque nam sed nullam sed diam turpis ipsum eu a sed convallis
-            diam.
-          </p>
-          <blockquote>
+          </div>
+          <div className='mt-6 prose prose-pink prose-lg text-gray-500 mx-auto'>
             <p>
-              Sagittis scelerisque nulla cursus in enim consectetur quam. Dictum
-              urna sed consectetur neque tristique pellentesque. Blandit amet,
-              sed aenean erat arcu morbi.
+              Faucibus commodo massa rhoncus, volutpat.{' '}
+              <strong>Dignissim</strong> sed. Mattis mauris semper sed amet
+              vitae sed turpis id. Id dolor praesent donec est. Odio penatibus
+              risus viverra tellus varius sit neque erat velit. Faucibus commodo
+              massa rhoncus, volutpat. Dignissim sed eget risus enim.{' '}
+              <a href='#'>Mattis mauris semper</a> sed amet vitae sed turpis id.
             </p>
-          </blockquote>
-          <p>
-            Faucibus commodo massa rhoncus, volutpat. Dignissim sed eget risus
-            enim. Mattis mauris semper sed amet vitae sed turpis id. Id dolor
-            praesent donec est. Odio penatibus risus viverra tellus varius sit
-            neque erat velit.
-          </p>
-          <figure>
-            <img
-              className='w-full rounded-lg'
-              src='https://images.unsplash.com/photo-1530893609608-32a9af3aa95c?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1000&q=80'
-              alt=''
-              width={1000}
-              height={600}
-            />
-            <figcaption>
-              Sagittis scelerisque nulla cursus in enim consectetur quam.
-            </figcaption>
-          </figure>
-          <h2>A thriving community of developers</h2>
-          <p>
-            Purus morbi dignissim senectus mattis <a href='#'>adipiscing</a>.
-            Amet, massa quam varius orci dapibus volutpat cras. In amet eu
-            ridiculus leo sodales cursus tristique. Tincidunt sed tempus ut
-            viverra ridiculus non molestie. Gravida quis fringilla amet eget dui
-            tempor dignissim. Facilisis auctor venenatis varius nunc, congue
-            erat ac. Cras fermentum convallis quam.
-          </p>
-          <p>
-            Faucibus commodo massa rhoncus, volutpat. Dignissim sed eget risus
-            enim. Mattis mauris semper sed amet vitae sed turpis id. Id dolor
-            praesent donec est. Odio penatibus risus viverra tellus varius sit
-            neque erat velit.
-          </p>
+            <ul>
+              <li>Quis elit egestas venenatis mattis dignissim.</li>
+              <li>
+                Cras cras lobortis vitae vivamus ultricies facilisis tempus.
+              </li>
+              <li>Orci in sit morbi dignissim metus diam arcu pretium.</li>
+            </ul>
+            <p>
+              Quis semper vulputate aliquam venenatis egestas sagittis quisque
+              orci. Donec commodo sit viverra aliquam porttitor ultrices gravida
+              eu. Tincidunt leo, elementum mattis elementum ut nisl, justo,
+              amet, mattis. Nunc purus, diam commodo tincidunt turpis. Amet,
+              duis sed elit interdum dignissim.
+            </p>
+            <h2>From beginner to expert in 30 days</h2>
+            <p>
+              Id orci tellus laoreet id ac. Dolor, aenean leo, ac etiam
+              consequat in. Convallis arcu ipsum urna nibh. Pharetra, euismod
+              vitae interdum mauris enim, consequat vulputate nibh. Maecenas
+              pellentesque id sed tellus mauris, ultrices mauris. Tincidunt enim
+              cursus ridiculus mi. Pellentesque nam sed nullam sed diam turpis
+              ipsum eu a sed convallis diam.
+            </p>
+            <blockquote>
+              <p>
+                Sagittis scelerisque nulla cursus in enim consectetur quam.
+                Dictum urna sed consectetur neque tristique pellentesque.
+                Blandit amet, sed aenean erat arcu morbi.
+              </p>
+            </blockquote>
+            <p>
+              Faucibus commodo massa rhoncus, volutpat. Dignissim sed eget risus
+              enim. Mattis mauris semper sed amet vitae sed turpis id. Id dolor
+              praesent donec est. Odio penatibus risus viverra tellus varius sit
+              neque erat velit.
+            </p>
+            <figure>
+              <img
+                className='w-full rounded-lg'
+                src='https://images.unsplash.com/photo-1531297484001-80022131f5a1?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1000&q=80'
+                alt=''
+                width={1000}
+                height={600}
+              />
+              <figcaption>
+                Sagittis scelerisque nulla cursus in enim consectetur quam.
+              </figcaption>
+            </figure>
+            <h2>Everything you need to get up and running</h2>
+            <p>
+              Purus morbi dignissim senectus mattis <a href='#'>adipiscing</a>.
+              Amet, massa quam varius orci dapibus volutpat cras. In amet eu
+              ridiculus leo sodales cursus tristique. Tincidunt sed tempus ut
+              viverra ridiculus non molestie. Gravida quis fringilla amet eget
+              dui tempor dignissim. Facilisis auctor venenatis varius nunc,
+              congue erat ac. Cras fermentum convallis quam.
+            </p>
+            <p>
+              Faucibus commodo massa rhoncus, volutpat. Dignissim sed eget risus
+              enim. Mattis mauris semper sed amet vitae sed turpis id. Id dolor
+              praesent donec est. Odio penatibus risus viverra tellus varius sit
+              neque erat velit.
+            </p>
+            <p>
+              Faucibus commodo massa rhoncus, volutpat.{' '}
+              <strong>Dignissim</strong> sed. Mattis mauris semper sed amet
+              vitae sed turpis id. Id dolor praesent donec est. Odio penatibus
+              risus viverra tellus varius sit neque erat velit. Faucibus commodo
+              massa rhoncus, volutpat. Dignissim sed eget risus enim.{' '}
+              <a href='#'>Mattis mauris semper</a> sed amet vitae sed turpis id.
+            </p>
+            <ul>
+              <li>Quis elit egestas venenatis mattis dignissim.</li>
+              <li>
+                Cras cras lobortis vitae vivamus ultricies facilisis tempus.
+              </li>
+              <li>Orci in sit morbi dignissim metus diam arcu pretium.</li>
+            </ul>
+            <p>
+              Quis semper vulputate aliquam venenatis egestas sagittis quisque
+              orci. Donec commodo sit viverra aliquam porttitor ultrices gravida
+              eu. Tincidunt leo, elementum mattis elementum ut nisl, justo,
+              amet, mattis. Nunc purus, diam commodo tincidunt turpis. Amet,
+              duis sed elit interdum dignissim.
+            </p>
+            <h2>The help you need any time you feel stuck</h2>
+            <p>
+              Id orci tellus laoreet id ac. Dolor, aenean leo, ac etiam
+              consequat in. Convallis arcu ipsum urna nibh. Pharetra, euismod
+              vitae interdum mauris enim, consequat vulputate nibh. Maecenas
+              pellentesque id sed tellus mauris, ultrices mauris. Tincidunt enim
+              cursus ridiculus mi. Pellentesque nam sed nullam sed diam turpis
+              ipsum eu a sed convallis diam.
+            </p>
+            <blockquote>
+              <p>
+                Sagittis scelerisque nulla cursus in enim consectetur quam.
+                Dictum urna sed consectetur neque tristique pellentesque.
+                Blandit amet, sed aenean erat arcu morbi.
+              </p>
+            </blockquote>
+            <p>
+              Faucibus commodo massa rhoncus, volutpat. Dignissim sed eget risus
+              enim. Mattis mauris semper sed amet vitae sed turpis id. Id dolor
+              praesent donec est. Odio penatibus risus viverra tellus varius sit
+              neque erat velit.
+            </p>
+            <figure>
+              <img
+                className='w-full rounded-lg'
+                src='https://images.unsplash.com/photo-1530893609608-32a9af3aa95c?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1000&q=80'
+                alt=''
+                width={1000}
+                height={600}
+              />
+              <figcaption>
+                Sagittis scelerisque nulla cursus in enim consectetur quam.
+              </figcaption>
+            </figure>
+            <h2>A thriving community of developers</h2>
+            <p>
+              Purus morbi dignissim senectus mattis <a href='#'>adipiscing</a>.
+              Amet, massa quam varius orci dapibus volutpat cras. In amet eu
+              ridiculus leo sodales cursus tristique. Tincidunt sed tempus ut
+              viverra ridiculus non molestie. Gravida quis fringilla amet eget
+              dui tempor dignissim. Facilisis auctor venenatis varius nunc,
+              congue erat ac. Cras fermentum convallis quam.
+            </p>
+            <p>
+              Faucibus commodo massa rhoncus, volutpat. Dignissim sed eget risus
+              enim. Mattis mauris semper sed amet vitae sed turpis id. Id dolor
+              praesent donec est. Odio penatibus risus viverra tellus varius sit
+              neque erat velit.
+            </p>
+          </div>
         </div>
       </div>
-    </div>
+      <ScrollToTop />
+    </>
   );
 }
